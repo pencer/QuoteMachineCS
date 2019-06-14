85d84e4ab4cc63b34c00b314d1cef92f7457346c
Yuki Kobayashi <kobayashi.yuki@gmail.com>
2019-06-14 11:16:44 +0900

    Added a new feature to convert a path to OneNote page for pasting on Slack.  * remove "///"  * replace a backslash with a slash

--- QuoteMachineCS/Form1.cs
+++ QuoteMachineCS/Form1.cs
@@ -22,6 +22,7 @@ namespace QuoteMachineCS
 
         const int HOTKEY_ID1  = 0x0001;
         const int HOTKEY_ID2  = 0x0002;
+        const int HOTKEY_ID3  = 0x0003;
 
         [DllImport("user32.dll")]
         extern static int RegisterHotKey(IntPtr HWnd, int ID, int MOD_KEY, int KEY);
@@ -86,11 +87,41 @@ namespace QuoteMachineCS
             }
         }
 
+        private void ConvOneNotePathForSlack()
+        {
+            IDataObject data = Clipboard.GetDataObject();
+            if (data.GetDataPresent(DataFormats.Text))
+            {
+                string str = (string)data.GetData(DataFormats.Text);
+                string[] lines = str.Split('\n');
+                string res = "";
+                bool first = true;
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string line = lines[i];
+                    if ((line == "") && (i == lines.Length - 1)) { break; }
+                    if (!first) { res += "\n"; }
+                    if (line.StartsWith("onenote:///"))
+                    {
+                        res += "onenote:" + line.Substring(11).Replace('\\', '/');
+                    }
+                    else
+                    {
+                        res += line;
+                    }
+                    first = false;
+                }
+                Clipboard.SetDataObject(res, true);
+            }
+
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
             // Register Hotkey
             RegisterHotKey(this.Handle, HOTKEY_ID1, MOD_CONTROL, (int)Keys.F9);
             RegisterHotKey(this.Handle, HOTKEY_ID2, MOD_CONTROL, (int)Keys.F8);
+            RegisterHotKey(this.Handle, HOTKEY_ID3, MOD_CONTROL, (int)Keys.F7);
 
             // Hide in task tray
             // http://csharp-cafe.info/c/c%E3%81%A7%E3%82%BF%E3%82%B9%E3%82%AF%E3%83%88%E3%83%AC%E3%82%A4%E5%B8%B8%E9%A7%90%E5%9E%8B%E3%82%A2%E3%83%97%E3%83%AA%E4%BD%9C%E6%88%90%E6%B3%952.html
@@ -102,6 +133,7 @@ namespace QuoteMachineCS
             // Unregister Hotkey
             UnregisterHotKey(this.Handle, HOTKEY_ID1);
             UnregisterHotKey(this.Handle, HOTKEY_ID2);
+            UnregisterHotKey(this.Handle, HOTKEY_ID3);
         }
 
         protected override void WndProc(ref Message m)
@@ -119,6 +151,11 @@ namespace QuoteMachineCS
                 {
                     RemoveQuote();
                     this.notifyIcon1.BalloonTipText = "Quote removed.";
+                }
+                if ((int)m.WParam == HOTKEY_ID3)
+                {
+                    ConvOneNotePathForSlack();
+                    this.notifyIcon1.BalloonTipText = "Converted OneNote Path for Slack.";
                 }
             }
         }
